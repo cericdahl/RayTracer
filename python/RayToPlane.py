f60df3f3f9690dee8b5ac27909f4d7960adff40f
cericdahl <41292578+cericdahl@users.noreply.github.com>
2020-07-14 13:41:02 -0500

    Using better indexing in RayToPlane.py
    Not tested yet.

--- python/RayToPlane.py
+++ python/RayToPlane.py
@@ -28,12 +28,14 @@ def RayToPlane(starting_points, indir, plane_pt, plane_norm):
     # Find intersection points
     # distance_traveled = ((repmat(plane_point, numrays, 1) - starting_points) * plane_normal') ./ (incoming_directions * plane_normal');
     with np.errstate(divide='ignore', invalid='ignore'):
-        distance_traveled = ((np.matlib.repmat(plane_pt, numrays, 1) - starting_points) * plane_norm) / (indir * plane_norm)
+        distance_traveled = np.sum((plane_pt - starting_points) * plane_norm, axis=1, keepdims=True) / np.sum(indir * plane_norm, axis=1, keepdims=True)
+        # distance_traveled = ((np.matlib.repmat(plane_pt, numrays, 1) - starting_points) * plane_norm) / (indir * plane_norm)
 
     # Removed repmat for distance_traveled and np.sign(indir * plane_norm), were already nx3; should it be nx3 or nx1?
-    intersection_points = starting_points + distance_traveled * indir
-    surface_normals = -np.matlib.repmat(plane_norm, numrays, 1) * np.sign(indir * np.transpose(plane_norm))
-    crossing_into = np.round_(-np.sign(indir * np.transpose(plane_norm)))
+    intersection_points = starting_points(:,:,np.newaxis) + distance_traveled(:,np.newaxis,:) * indir(:,:,np.newaxis)
+    surface_normals = -plane_norm * np.sign(np.sum(indir * plane_norm), axis=1, keepdims=True)
+    # surface_normals = -np.matlib.repmat(plane_norm, numrays, 1) * np.sign(indir * np.transpose(plane_norm))
+    crossing_into = np.round_(-np.sign(np.sum(indir * np.transpose(plane_norm), axis=1, keepdims=True)))
 
     return [intersection_points, surface_normals, distance_traveled, crossing_into]
 
