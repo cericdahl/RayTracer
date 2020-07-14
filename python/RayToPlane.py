f4b022758657a0af88b44d78a25b7e7bf73e196a
Ari Sloss <arisloss2023@u.northwestern.edu>
2020-07-14 13:55:19 -0400

    update

---
+++ python/RayToPlane.py
@@ -0,0 +1,39 @@
+import numpy as np
+import numpy.matlib
+
+
+def RayToPlane(starting_points, indir, plane_pt, plane_norm):
+
+    intersection_points = []
+    surface_normals = []
+    distance_traveled = []
+    crossing_into = []
+
+    # Check inputs
+    if len(plane_pt) != 3 or len(plane_norm) != 3 or starting_points.shape[1] != 3 or indir.shape[1] != 3 or starting_points.shape[0] != indir.shape[0]:
+        raise Exception('Improper input to RayToPlane')
+    plane_pt = np.transpose(plane_pt)
+    plane_norm = np.transpose(plane_norm)
+    numrays = starting_points.shape[0]
+
+    # Normalize directions | some of this should probably be done in RayToShape as it is common among all RayToXXX
+    goodray_cut = np.sum(indir ** 2, 1) > 0
+    if any(goodray_cut):
+        indir[goodray_cut, :] = indir[goodray_cut, :] / np.transpose(np.matlib.repmat(abs(np.sqrt(np.sum(indir ** 2, 1))), 3, 1))
+    if sum(plane_norm ** 2) > 0:
+        plane_norm = plane_norm / abs(np.sqrt(np.sum(plane_norm**2)))
+    else:
+        raise Exception('Invalid plane for RayToPlane')
+
+    # Find intersection points
+    # distance_traveled = ((repmat(plane_point, numrays, 1) - starting_points) * plane_normal') ./ (incoming_directions * plane_normal');
+    with np.errstate(divide='ignore', invalid='ignore'):
+        distance_traveled = ((np.matlib.repmat(plane_pt, numrays, 1) - starting_points) * plane_norm) / (indir * plane_norm)
+
+    # Removed repmat for distance_traveled and np.sign(indir * plane_norm), were already nx3; should it be nx3 or nx1?
+    intersection_points = starting_points + distance_traveled * indir
+    surface_normals = -np.matlib.repmat(plane_norm, numrays, 1) * np.sign(indir * np.transpose(plane_norm))
+    crossing_into = np.round_(-np.sign(indir * np.transpose(plane_norm)))
+
+    return [intersection_points, surface_normals, distance_traveled, crossing_into]
+
---
+++ python/TestGeometry.py
@@ -0,0 +1,108 @@
+# RayTracer2 Python port testing suite
+import numpy as np
+import math
+import random
+import RayTracer2
+import surface
+
+
+def TestGeometry():
+    # First, create ray starting points and isotropic rays
+    # Coordinates of startingpoint -- is the same for all rays
+    x = 0
+    y = 0
+    z = 2
+
+    n = 1#000  # number of rays
+
+    ray_startpoints = np.empty((n, 3))
+    ray_startpoints[..., 0] = x
+    ray_startpoints[..., 1] = y
+    ray_startpoints[..., 2] = z
+
+    # rays = initial [forward direction (3), s1 polarization axis (3), s0, s1, s2, s3]
+    # Let s0 be 1 and rays be unpolarized (other Stokes parameters = 0 + no polarization axis)
+    test_rays = np.zeros((n, 10))
+    test_rays[..., 6] = 1
+
+    # Assign initial forward directions in spherical coords for easier isotropism
+    for i in range(n):
+        azimuth = random.random() * 2 * math.pi
+        a = np.random.uniform(-1, 1)
+        polar = np.arccos(a) # -1 < theta < 1 so random numbers are not biased towards the poles
+
+        test_rays[i, 0] = np.sin(polar) * np.cos(azimuth) # x
+        test_rays[i, 1] = np.sin(polar) * np.sin(azimuth) # y
+        test_rays[i, 2] = np.cos(polar)  # z
+
+    # Test geometry consists of 5 surfaces
+    surface_list = []
+    # Bottom cylinder
+    bot_cyl = surface.surface()
+    bot_cyl.description = 'bottom cylinder along z-axis, 10cm radius from z=0 to z=5'
+    bot_cyl.shape = 'cylinder'
+    bot_cyl.param_list = [np.array([0, 0, 0]), np.array([0, 0, 1]), 10]
+    bot_cyl.inbounds_function = lambda p: np.reshape((p[:, 2, :] > 0) * (p[:, 2, :] < 5), (np.size(p, 0), -1)) # Replaced 3 w/ 2
+    bot_cyl.n_outside = np.inf
+    bot_cyl.n_inside = 1.5
+    bot_cyl.surface_type = 'normal'
+    bot_cyl.absorption = 0
+    surface_list.append(bot_cyl)
+
+    # Top cylinder
+    top_cyl = surface.surface()
+    top_cyl.description = 'top cylinder along z-axis, 10cm radius from z=5 to z=10'
+    top_cyl.shape = 'cylinder'
+    top_cyl.param_list = [np.array([0, 0, 0]), np.array([0, 0, 1]), 10]
+    top_cyl.inbounds_function = lambda p: np.reshape((p[:, 2, :] > 5) * (p[:, 2, :] < 10), (np.size(p, 0), -1))
+    top_cyl.n_outside = np.inf
+    top_cyl.n_inside = 2
+    top_cyl.surface_type = 'normal'
+    top_cyl.absorption = 0
+    surface_list.append(top_cyl)
+
+    # Top cap
+    top = surface.surface()
+    top.description = 'top cap, disk centered on z-axis with radius 10 and z=10'
+    top.shape = 'plane'
+    top.param_list = [np.array([0, 0, 10]), np.array([0, 0, 1])]
+    top.inbounds_function = lambda p: np.reshape((p[:, 0] ** 2 + p[:, 1] ** 2) < 100, (p.shape[0], -1)) # shifted indices down 1 and eliminated 3rd dimension index
+    # Direction of normal vector considered 'outside'
+    top.n_outside = np.inf
+    top.n_inside = 2
+    top.surface_type = 'normal'
+    top.absorption = 1
+    surface_list.append(top)
+
+    # Middle
+    mid = surface.surface()
+    mid.description = 'middle, disk centered on z-axis with radius 10 and z=5'
+    mid.shape = 'plane'
+    mid.param_list = [np.array([0, 0, 5]), np.array([0, 0, 1])]
+    mid.inbounds_function = lambda p: np.reshape((p[:, 1, :] ** 2 + p[:, 2, :] ** 2 < 100), (np.size(p, 0), -1))
+    mid.n_outside = 2
+    mid.n_inside = 1.5
+    mid.surface_type = 'normal'
+    mid.absorption = 0
+    surface_list.append(mid)
+
+    # Bottom cap
+    bottom = surface.surface()
+    bottom.description = 'bottom cap, disk centered on z-axis with radius 10 and z=0'
+    bottom.shape = 'plane'
+    bottom.param_list = [np.array([0, 0, 0]), np.array([0, 0, 1])]
+    bottom.inbounds_function = lambda p: np.reshape((p[:, 1, :] ** 2 + p[:, 2, :] ** 2 < 100), (np.size(p, 0), -1))
+    bottom.n_outside = 1.5
+    bottom.n_inside = np.inf
+    bottom.surface_type = 'normal'
+    bottom.absorption = 1
+    surface_list.append(bottom)
+
+    return ray_startpoints, test_rays, surface_list
+
+
+[starts, rays, surfaces] = TestGeometry()
+
+# return of RayTracer2: [ray_interfaces, absorption_table, raytable]
+
+[ray_interfaces, absorption_table, raytable] = RayTracer2.RayTracer2(starts, rays, surfaces)
